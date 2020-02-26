72841957a4adb34831dda10bb07b92163c186808
dongsub-joung <59364300+dongsub-joung@users.noreply.github.com>
2020-02-26 23:26:09 +0900

    1

--- UI/src/CoverF.java
+++ UI/src/CoverF.java
@@ -13,12 +13,16 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.InputMethodListener;
+import java.awt.event.InputMethodEvent;
+import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class CoverF extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -30,10 +34,7 @@ public class CoverF extends JFrame {
 					//외부 프레임
 					CoverF frame = new CoverF();
 					frame.setVisible(true);
-					
-					
-					
-					
+															
 					
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -42,10 +43,19 @@ public class CoverF extends JFrame {
 		});
 	}
 
+	
+	//그리고 이 값을 DBMS에 보내서 검색 시도하는 매서드
+	//DBMS의 검색결과를 가져오는 메서드
+	//textArea.setText(t); 의 class를 만듬
+	
+	
+
+	
 	/**
 	 * Create the frame.
 	 */
 	public CoverF() {
+		
 		//외부 프레임
 		setTitle("\uC0AC\uD68C\uD559\uACFC \uB3C4\uC11C\uAD00\uB9AC \uD504\uB85C\uADF8\uB7A8");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,21 +65,24 @@ public class CoverF extends JFrame {
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
 		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 				//외부 프레임 끝
+		
 		
 		//도서 검색 버튼
 		JButton btnNewButton = new JButton("\uAC80\uC0C9");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				//그리고 이 값을 DBMS에 보내서 검색 시도하는 매서드
+				//DBMS의 검색결과를 가져오는 메서드
+				//textArea.setText(t);
 			}
 		});
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
 		gbc_btnNewButton.gridx = 0;
 		gbc_btnNewButton.gridy = 1;
 		contentPane.add(btnNewButton, gbc_btnNewButton);
@@ -78,6 +91,18 @@ public class CoverF extends JFrame {
 		
 		//도서 검색 텍스트 창
 		textField = new JTextField();
+		textField.addInputMethodListener(new InputMethodListener() {
+			public void caretPositionChanged(InputMethodEvent event) {
+			}
+			public void inputMethodTextChanged(InputMethodEvent event) {		
+				textField.getText();
+				//그리고 이 값을 DBMS에 보내서 검색 시도하는 매서드
+				//DBMS의 검색결과를 가져오는 메서드
+				//textArea.setText(t);
+			}
+		});
+		
+		
 		textField.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -86,8 +111,10 @@ public class CoverF extends JFrame {
 			}
 		});
 		
+		
 		//도서검색창 프레임
 		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.gridx = 1;
 		gbc_textField.gridy = 1;
@@ -95,6 +122,16 @@ public class CoverF extends JFrame {
 		textField.setColumns(10);
 		textField.setText("도서명을 입력해주세요.");
 				//텍스트 창 끝
+		
+		
+		//결과 표시
+		textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 1;
+		gbc_textArea.gridy = 3;
+		contentPane.add(textArea, gbc_textArea);
+				//결과 표시 끝
 				
 		
 	
