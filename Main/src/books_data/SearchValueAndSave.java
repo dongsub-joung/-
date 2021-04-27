00001a7237ce03177013ccbfe5fc30f9c4c49326
dongsub-joung <joungdongsub1103@gmail.com>
2021-04-28 02:32:14 +0900

    Refactoring Search algoritm && valiable

--- Main/src/books_data/SearchValueAndSave.java
+++ Main/src/books_data/SearchValueAndSave.java
@@ -3,29 +3,38 @@ package books_data;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import Init.COMMON;
+import java.util.HashMap;
+
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import Init.COMMON;
 import static DataParsing.ParsingBookData.dataType;
 
+/*
+	1. Get String entered by user.
+	2. DataArray.bookNames().indexOf(String str)
+	3. Adjust SearchValueAndSave's parameter.
+	4. Set HashMap obj_searched_book
+ */
 
 public class SearchValueAndSave
 {
+	private static HashMap obj_searched_book= new HashMap();
 
-	SearchValueAndSave(){}
+//	Search Row using bookIndex, than save values in obj_searched_book.
 	public SearchValueAndSave(int bookIndex)
 	{
+		final int SELECTED_ROW= bookIndex+3;
 		String value= "";
 
 		try(FileInputStream file = new FileInputStream(COMMON.FILE_PATH);
 			@SuppressWarnings("resource")
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
-			XSSFSheet sheet = workbook.getSheetAt(0);
-			XSSFRow row = sheet.getRow(bookIndex+3))
+			XSSFSheet sheet = workbook.getSheetAt(COMMON.ABOUT_BOOK_SHEET);
+			XSSFRow row = sheet.getRow(SELECTED_ROW))
 		{
 			if(row != null)
 			{
@@ -40,23 +49,25 @@ public class SearchValueAndSave
 						switch(columnIndex)
 						{
 							case 1:
-								DataArray.getBookNames().add(value);
+								obj_searched_book.put("BookName", value);
 								break;
 							case 2:
-								DataArray.getPublishingCompanys().add(value);
+								obj_searched_book.put("PublishingCompanys", value);
 								break;
 							case 3:
-								DataArray.getAuthors().add(value);
+								obj_searched_book.put("Authors", value);
 								break;
 							case 4:
-								DataArray.getTranslators().add(value);
+								obj_searched_book.put("Translators", value);
 								break;
 							case 7:
-								DataArray.getAmounts().add(value);
+								obj_searched_book.put("Amounts", value);
 								break;
 							case 8:
-								DataArray.getLocations().add(value);
+								obj_searched_book.put("Locations", value);
 								break;
+							default:
+								System.out.println("Out Of Range"); break;
 						}
 					}
 					else if (cell == null) { continue; }
@@ -66,5 +77,9 @@ public class SearchValueAndSave
 		catch (FileNotFoundException e){ System.out.println("file path err"); e.printStackTrace(); }
 		catch (IOException e) { System.out.println("I/O err"); e.printStackTrace(); }
 		catch (Exception e) { System.out.println(e + "unexpected err"); }
+	}
+
+	public static HashMap getObj_searched_book() {
+		return obj_searched_book;
 	}
 }
