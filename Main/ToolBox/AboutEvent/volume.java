7031c97a1f15e180ad9c71c928eee2a0e090158f
DongSub_Joung <ehdtjqwjd@kangwon.ac.kr>
2021-12-19 20:18:53 +0900

    END OF FEEDBACK

--- Main/ToolBox/AboutEvent/volume.java
+++ Main/ToolBox/AboutEvent/volume.java
@@ -5,14 +5,28 @@ public class volume{
     public final ZERO= -1;
     private int[100] vol;
     
-
     public void setVol(){
         for(int i=0; i<100; i++) vol[i]= 0;
     }
 
-    // When voume is above zero, block a 대출 btn
+    public void mergingData(){
+        // save files
+    }
+
+    // (searching own book) When voume is above zero, block a 대출 btn
     public void inforceZero(int index){
+        // GUI: Event Handler
+        // Another way: Making "Up && Down btn" and set minimum value as "0" 
+    }
+
+    public void turnOn_ZERO(){
         int current_state= vol[index];
-        if ()
+        if (current_state == 0) {
+            // Show at searching bar -1 code
+            print(ZERO);
+        } else {
+            // Show at searching bar "stock voume"
+            print(current_state);
+        }
     }
 }
